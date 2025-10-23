f087ff4a2dcb4eab524350eca839abb7dfb972d2
jinho3085 <jinho3085@naver.com>
2025-10-23 15:57:17 +0900

    01.Basics finsih

---
+++ 01.Basics/ex04/src/app.js
@@ -0,0 +1,9 @@
+import './assets/css/App.css';
+function App() {
+    const App = document.createElement('div');
+    App.textContent = 'hello, world';
+    App.className = 'Header';
+    return App;
+}
+
+export {App};
