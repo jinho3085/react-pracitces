a2c053b857461c07517f2fdd9802a3cfd43ec562
jinho3085 <jinho3085@naver.com>
2025-11-04 09:12:22 +0900

    Roting finsh

---
+++ 09. Routing/ex04/src/component/Guestbook.js
@@ -0,0 +1,30 @@
+import React, { useEffect } from 'react';
+import {Link, NavLink} from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
+
+export default function Guestbook() {
+    const navigate = useNavigate();
+
+    useEffect(()=> {
+        setTimeout(()=>{
+            // access denided 응답을 받은 경우,
+
+            // X 
+            // window.location.href = '/error/401';
+
+            // O
+            Navigate('/error/401');
+        }, 3000);
+    }, []);
+
+    return (
+        <div>
+            <h1>Guestbook</h1>
+            <ul>
+                <li><Link className={'menu'} to={'/'}>[Main]</Link></li>
+                <li><NavLink className={'menu'} to={'/guestbook'}>[Guestbook]</NavLink></li>
+                <li><NavLink to={'/gallery'}>[Gallery]</NavLink></li>
+            </ul>
+        </div>
+    );
+}
---
+++ 09. Routing/ex07/src/layout/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+import '../assets/scss/layout/Header.scss';
+
+export default function Header() {
+    
+    console.log('render Header....');
+
+    return (
+        <header>
+            <h1>Header</h1>
+            <ul>
+                <li>
+                    <NavLink to={'/user/login'}>Login</NavLink>
+                </li>
+                <li>
+                    <NavLink to={'/user/join'}>Join</NavLink>
+                </li>
+                <li>
+                    <NavLink to={'/user/settings'}>settings</NavLink>
+                </li>
+            </ul>
+        </header>
+    );
+}
